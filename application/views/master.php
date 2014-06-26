665c4e8164f14d3bc09de76785501e6a9c0f8c19
Soyo Solution <xenialaw418@gmail.com>
2014-06-26 10:56:33 +0800

    Change all <? to <?php
    Change all <? to <?php for fixing bugs

--- application/views/master.php
+++ application/views/master.php
@@ -2,21 +2,21 @@
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<title> <? echo $site_title;?> </title>
-	<link href="<? echo $base_url;?>css/style.css" rel="stylesheet" type="text/css" />
+	<title> <?php echo $site_title;?> </title>
+	<link href="<?php echo $base_url;?>css/style.css" rel="stylesheet" type="text/css" />
 </head>
 <body>      
 <div class="header">   
   <div class="nav-bar left">
    <ul class="button-group">
-	 <li><a href="<? echo $site_url;?>/home" class="button"><? echo $this->menu_data['home'];?></a></li>
-	 <li><a href="<? echo $site_url;?>/home/latest" class="button"><? echo $this->menu_data['latest_news'];?></a></li>
-	 <li><a href="<? echo $site_url;?>/contact" class="button"><? echo $this->menu_data['contact'];?></a></li>
+	 <li><a href="<?php echo $site_url;?>/home" class="button"><?php echo $this->menu_data['home'];?></a></li>
+	 <li><a href="<?php echo $site_url;?>/home/latest" class="button"><?php echo $this->menu_data['latest_news'];?></a></li>
+	 <li><a href="<?php echo $site_url;?>/contact" class="button"><?php echo $this->menu_data['contact'];?></a></li>
 	</ul>
   </div>
   <div class="lang-bar right">
 	<ul class="button-group">
-	<? 
+	<?php 
 	if(count($this->site_data['site_language'])>0){
 		foreach($this->site_data['site_language'] as $code=>$language){
 			echo '<li><a href="'.$base_url.$code."/".$this->current_page.'">'.$language.'</a></li>';
@@ -25,7 +25,7 @@
   ?></ul></div>
 </div>
 <div class="content">
- <? echo $main_content;?>
+ <?php echo $main_content;?>
 </div>
 </body>
 </html> 
